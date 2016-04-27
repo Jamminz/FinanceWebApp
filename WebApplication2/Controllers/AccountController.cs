3b60e6389314aacc1c581be5c36ff337c668478e
jim <jimgotslim@hotmail.com>
2016-04-27 13:16:08 +0100

    Inital Commit

---
+++ WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (ApplicationDbContext db = new ApplicationDbContext())
+                {
+                    string username = user.UserName;
+                    string password = user.Password;
+                    bool userValid = db.Users.Any(o => o.UserName == username && user.Password == password);
+
+                    if (userValid)
+                    {
+                        FormsAuthentication.SetAuthCookie(username, false);
+                        Session["UserID"] = username;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else ModelState.AddModelError("", "The user name or password does not exist");
+                }
+            }
+
+            return View();
+        }
+
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            Session["UserID"] = null;
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
